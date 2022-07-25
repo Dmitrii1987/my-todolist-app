64fe36f75caad8c240054c78e160741082271e85
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2022-07-26 00:01:06 +0300

    feat: update EdiatbleSpan component

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type EditableSpanType = {
     value: string
 }
 
 const EditableSpan = (props: EditableSpanType) => {
-    return (
-        <span>
-            {props.value}
-        </span>
-    );
+
+    const [mode,setMode] = useState<boolean>(false)
+
+    const editMode = ()=> {
+        if (activateEditMode()) {
+            setMode(!mode)
+        }
+    }
+
+    const activateEditMode = () => {
+        setMode(!mode)
+    }
+
+    return editMode ? <input value={props.value}/> : <span onDoubleClick={activateEditMode} >{props.value}</span>
+    
 };
 
 export default EditableSpan;
