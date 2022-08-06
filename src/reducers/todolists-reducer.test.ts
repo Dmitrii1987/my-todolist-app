c5460d04004b09b5565e0308a3ca6c852001e632
Dmitrii_K <dmitrii.kuzminyh@gmail.com>
2022-08-06 09:51:12 +0300

    feat: update tests

--- src/reducers/todolists-reducer.test.ts
+++ src/reducers/todolists-reducer.test.ts
@@ -2,15 +2,32 @@ import { v1 } from "uuid"
 import { TodolistsType } from "../App"
 import { todolistsReducer } from "./todolists-reducer"
 
-test('correct todolist should be removed',()=>{
-    let todolistID1 = v1()
-    let todolistID2 = v1()
+// test('correct todolist should be removed',()=>{
+//     let todolistID1 = v1()
+//     let todolistID2 = v1()
 
-    const startState : Array<TodolistsType> = [
-        { id: todolistID1, title: 'What to learn', filter: 'all' },
-        { id: todolistID2, title: 'What to buy', filter: 'all' }
+//     const startState : Array<TodolistsType> = [
+//         { id: todolistID1, title: 'What to learn', filter: 'all' },
+//         { id: todolistID2, title: 'What to buy', filter: 'all' }
+//     ]
+//     const endState = todolistsReducer(startState,{type: 'REMOVE-TODOLIST',id:todolistID1})
+//         expect(endState.length).toBe(1)
+//         expect(endState[0].id).toBe(todolistID2)
+// })
+
+test('correct todolist should be added', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistsType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
     ]
-    const endState = todolistsReducer(startState,{type: 'REMOVE-TODOLIST',id:todolistID1})
-        expect(endState.length).toBe(1)
-        expect(endState[0].id).toBe(todolistID2)
+
+    const endState = todolistsReducer(startState, {type: 'ADD-TODOLIST', title: newTodolistTitle})
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
 })
